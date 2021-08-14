90aa4102715b137021579d86f96760abff9f4ce5
wildpineweb <86894890+wildpineweb@users.noreply.github.com>
2021-08-13 22:10:50 -0700

    Project Complete
    8/13/21

--- app.js
+++ app.js
@@ -50,191 +50,6 @@ let toggleNav = function () {
     }
 }
 
-/* Modals collected in variables */
-
-// var $modalOne = $(".modal-one");
-// var $modalTwo = $(".modal-two");
-// var $modalThree = $(".modal-three");
-// var $modalFour = $(".modal-four");
-// var $modalFive = $(".modal-five");
-// var $modalSix = $(".modal-six");
-// var $modalSeven = $(".modal-seven");
-// var $modalEight = $(".modal-eight");
-// var $modalNine = $(".modal-nine");
-// var $modalTen = $(".modal-ten");
-// var $modalEleven = $(".modal-eleven");
-// var $modalTwelve = $(".modal-twelve");
-// var $modalThirteen = $(".modal-thirteen");
-// var $modalFourteen = $(".modal-fourteen");
-// var $modalFifteen = $(".modal-fifteen");
-// var $modalSixteen = $(".modal-sixteen");
-// var $modalSeventeen = $(".modal-seventeen");
-// var $modalEighteen = $(".modal-eighteen");
-// var $modalNineteen = $(".modal-nineteen");
-// var $modalTwenty = $(".modal-twenty");
-// var $modalTwentyOne = $(".modal-twenty-one");
-
-/* Images collected in variables */
-
-// var $homeIcon = $(".home-icon");
-// var $html = $("html");
-
-// var $one = $("#one");
-// var $two = $("#two");
-// var $three = $("#three");
-// var $four = $("#four");
-// var $five = $("#five");
-// var $six = $("#six");
-// var $seven = $("#seven");
-// var $eight = $("#eight");
-// var $nine = $("#nine");
-// var $ten = $("#ten");
-// var $eleven = $("#eleven");
-// var $twelve = $("#twelve");
-// var $thirteen = $("#thirteen");
-// var $fourteen = $("#fourteen");
-// var $fifteen = $("#fifteen");
-// var $sixteen = $("#sixteen");
-// var $seventeen = $("#seventeen");
-// var $eighteen = $("#eighteen");
-// var $nineteen = $("#nineteen");
-// var $twenty = $("#twenty");
-// var $twentyOne = $("#twenty-one");
-
-/* Add Clicked Class */
-
-// $one.on("click", function (e) {
-//   $modalOne.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $two.on("click", function (e) {
-//   $modalTwo.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $three.on("click", function (e) {
-//   $modalThree.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $four.on("click", function (e) {
-//   $modalFour.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $five.on("click", function (e) {
-//   $modalFive.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $six.on("click", function (e) {
-//   $modalSix.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $seven.on("click", function (e) {
-//   $modalSeven.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $eight.on("click", function (e) {
-//   $modalEight.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $nine.on("click", function (e) {
-//   $modalNine.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $ten.on("click", function (e) {
-//   $modalTen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $eleven.on("click", function (e) {
-//   $modalEleven.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $twelve.on("click", function (e) {
-//   $modalTwelve.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $thirteen.on("click", function (e) {
-//   $modalThirteen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $fourteen.on("click", function (e) {
-//   $modalFourteen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $fifteen.on("click", function (e) {
-//   $modalFifteen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $sixteen.on("click", function (e) {
-//   $modalSixteen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $seventeen.on("click", function (e) {
-//   $modalSeventeen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $eighteen.on("click", function (e) {
-//   $modalEighteen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $nineteen.on("click", function (e) {
-//   $modalNineteen.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $twenty.on("click", function (e) {
-//   $modalTwenty.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-// $twentyOne.on("click", function (e) {
-//   $modalTwentyOne.addClass("clicked");
-//   $html.addClass("clicked");
-// });
-
-/* Remove Clicked Class */
-
-// $homeIcon.on("click", function (e) {
-//   $modalOne.removeClass("clicked");
-//   $modalTwo.removeClass("clicked");
-//   $modalThree.removeClass("clicked");
-//   $modalFour.removeClass("clicked");
-//   $modalFive.removeClass("clicked");
-//   $modalSix.removeClass("clicked");
-//   $modalSeven.removeClass("clicked");
-//   $modalEight.removeClass("clicked");
-//   $modalNine.removeClass("clicked");
-//   $modalTen.removeClass("clicked");
-//   $modalEleven.removeClass("clicked");
-//   $modalTwelve.removeClass("clicked");
-//   $modalThirteen.removeClass("clicked");
-//   $modalFourteen.removeClass("clicked");
-//   $modalFifteen.removeClass("clicked");
-//   $modalSixteen.removeClass("clicked");
-//   $modalSeventeen.removeClass("clicked");
-//   $modalEighteen.removeClass("clicked");
-//   $modalNineteen.removeClass("clicked");
-//   $modalTwenty.removeClass("clicked");
-//   $modalTwentyOne.removeClass("clicked");
-//   $html.removeClass("clicked");
-// });
-
 /* Navigation */
 var $navbarMenu = $(".navbar-menu");
 var $sideNavA = $(".side-nav a");
@@ -242,29 +57,22 @@ var $isActiveHamburger = $(".hamburger.is-active .hamburger-inner::after");
 var $hamburgerInner = $(".hamburger-inner");
 var $mobileNav = $(".mobile-nav");
 
-/* Body and Core Elements */
-// var $body = $("body");
+/* SSN automated dashes in Application Form */
+const ssn = document.getElementById('ssn');
 
-// var $h2 = $("h2");
-// var $h3 = $("h3");
-// var $p = $("p");
+ssn.addEventListener("keydown", (e) => {
+    if(e.key === "Backspace" || e.key === "Delete") return;
+    if(e.target.value.length === 3) {
+        ssn.value = ssn.value + "-";
+    }
+    if(e.target.value.length === 6) {
+        ssn.value = ssn.value + "-";
+    }
+});
 
-// Label the different sectiona of your website with the elements you 
-// are targeting to chnage with a dark-mode class. Then when the first 
-// page is done, label a new section for the next page, and under that 
-// - label its sections as you did before. and repeat for each 
-
-// /* Landing */
-// var $clippy = $(".clippy");
-
-// // About 
-// var $topCard = $(".top-card");
-
-// // Services 
-// var $services = $("#services");
-// var $svgCircle = $(".svg-circle");
-// var $exph3 = $("#experience h4")
-
-// // Review 
-// var $reviewerInfo = $(".reviewer-info h4");
-// var $quotesBefore = $("#quote");
+/* SSN Number Only Requirement */
+function isNumber(e){
+    e = e || window.event;
+    var charCode = e.which ? e.which : e.keyCode;
+    return /\d/.test(String.fromCharCode(charCode));
+}
